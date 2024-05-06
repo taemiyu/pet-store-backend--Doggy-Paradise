03f129756281945f8006ac739175607f1c066f45
taemiyu <154125772+taemiyu@users.noreply.github.com>
2024-05-06 16:04:51 +0800

    Update CloudinaryConfig.java

--- src/main/java/com/ispan/dogland/config/cloudConfig/CloudinaryConfig.java
+++ src/main/java/com/ispan/dogland/config/cloudConfig/CloudinaryConfig.java
@@ -9,16 +9,48 @@ import java.util.Map;
 
 
 @Configuration
+@PropertySource("application.properties")
 public class CloudinaryConfig {
+
+    @Value("${cloud_name}")
+    private String cloudName;
+    @Value("${cloud_api_key}")
+    private String apiKey;
+    @Value("${cloud_api_secret}")
+    private String apiSecret;
 
 
     @Bean
     public Cloudinary getCloudinary(){
         Map config = new HashMap();
-        config.put("cloud_name","dxz9qtntt");
-        config.put("api_key","343193624619149");
-        config.put("api_secret","vqwpNIH-xiQ1mmkM4ZizeMgpaPI");
+        config.put("cloud_name",cloudName);
+        config.put("api_key",apiKey);
+        config.put("api_secret",apiSecret);
         config.put("secure",true);
         return  new Cloudinary(config);
+    }
+
+    public String getCloudName() {
+        return cloudName;
+    }
+
+    public void setCloudName(String cloudName) {
+        this.cloudName = cloudName;
+    }
+
+    public String getApiSecret() {
+        return apiSecret;
+    }
+
+    public void setApiSecret(String apiSecret) {
+        this.apiSecret = apiSecret;
+    }
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
     }
 }
